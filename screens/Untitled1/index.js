dc32eadc5d1f24e771a5fab1a85ab0d4fc4ea6bc
sneh1 Mishra1 <sneh1@crowdbotics.com>
2022-12-06 16:29:20 +0530

    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled1/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+const Untitled1 = () => {
+  return <View style={_styles.qfzEtyKE}>
+      <View style={_styles.xugZsKpV}></View></View>;
+};
+
+export default Untitled1;
+
+const _styles = StyleSheet.create({
+  qfzEtyKE: {
+    backgroundColor: "#f0f0f1",
+    padding: 10,
+    position: "relative",
+    height: "100%",
+    borderRadius: 0
+  },
+  xugZsKpV: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777"
+  }
+});
